50957b0069b1409679c441e42e47e579544cc602
koo-rogie <rn5184@naver.com>
2025-06-05 13:34:18 +0900

    📝 docs: 2025-06-05 1~3교시 중간 커밋

---
+++ workspace/ch02-start/props/10/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 임포트시  src부터 시작하는 절대경로로 지정해줌
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+    ],
+  },
+})
--- workspace/ch02-start/vite/09/src/pages/todo/List.tsx
+++ workspace/ch02-start/vite/09/src/pages/todo/List.tsx
@@ -1,12 +1,27 @@
+import type { TodoItem } from "../../App";
+/**
+ * 배열 list는 선언한곳이아니라 사용하는데서 타입정의를 해줘야하나요?
+ * 배열의 매개변수의 타입을 필수지정하도록 만들어져 있음
+ * 그리고 이 파일에서만 사용하기때문에 해당 파일에서 만들 수 있었음
+ * interface 옮겨도 되고 안해 되고..
+ */
+
+interface TodoListprops {
+  list: TodoItem[];
+}
+
 // Todo 목록을 전달받아서 출력해야 함
-function TodoList() {
-  return (
-    <ul className="todolist">
-      <li>React 공부 - 진행중</li>
-      <li>Javascript 프로젝트 - 완료</li>
-      <li>Javascript 공부 - 완료</li>
-    </ul>
-  );
+function TodoList({ list }: TodoListprops) {
+  const itemList = list.map((item) => {
+
+    return (
+      <li key={item._id}>
+        {item.title} - {item.done ? "완료" : "진행중"}
+      </li>
+    );
+  });
+  
+  return <ul className="todolist">{itemList}</ul>;
 }
 
 export default TodoList;
