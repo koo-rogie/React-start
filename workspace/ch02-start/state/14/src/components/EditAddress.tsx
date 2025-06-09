cfc64e53b06175efdb7322fd1a9c37b9e59de919
koo-rogie <rn5184@naver.com>
2025-06-09 17:04:12 +0900

    📝 docs: 2025-06-09 6~7교시 중간 커밋

--- workspace/ch02-start/state/14/src/components/EditAddress.tsx
+++ workspace/ch02-start/state/14/src/components/EditAddress.tsx
@@ -8,14 +8,15 @@ interface AddressBook {
 
 interface EditAddressProps {
   addressBook: AddressBook[];
+  handleAddressChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function EditAddress({ addressBook }: EditAddressProps) {
+function EditAddress({ addressBook, handleAddressChange }: EditAddressProps) {
   const list = addressBook.map((address: AddressBook) => {
     return (
       <Fragment key={address.id}>
         <label htmlFor={address.id.toString()}>{address.name}</label>
-        <input type="text" id={address.id.toString()} name={address.id.toString()} value={address.value} />
+        <input type="text" id={address.id.toString()} name={address.id.toString()} value={address.value} onChange={handleAddressChange} />
         <br />
       </Fragment>
     );
