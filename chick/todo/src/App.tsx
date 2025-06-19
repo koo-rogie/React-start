8211e2a80cbde7a171f92af735a9355ae79a2c4e
koo-rogie <rn5184@naver.com>
2025-06-19 17:06:17 +0900

    ➕add : 병아리반 파일 불러오기

--- chick/todo/src/App.tsx
+++ chick/todo/src/App.tsx
@@ -1,47 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Todo from "./pages/Todo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div id="todo">
-      <header>
-        <h1>Todo List - 🐤</h1>
-      </header>
-      <div id="main">
-        <h2>할일 목록</h2>
-        <div className="todoinput">
-          <input type="text" autoFocus />
-          <button type="button">추가</button>
-        </div>
-        <ul className="todolist">
-          <li>
-            <span>1</span>
-            <span>
-              <s>샘플1</s>
-            </span>
-            <button type="button">삭제</button>
-          </li>
-          <li>
-            <span>2</span>
-            <span>샘플2</span>
-            <button type="button">삭제</button>
-          </li>
-          <li>
-            <span>3</span>
-            <span>샘플3</span>
-            <button type="button">삭제</button>
-          </li>
-        </ul>
-      </div>
-      <footer>
-        <p>멋쟁이 사자처럼 FrontEnd BootCamp</p>
-      </footer>
+      <Header />
+      <Todo />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default App
