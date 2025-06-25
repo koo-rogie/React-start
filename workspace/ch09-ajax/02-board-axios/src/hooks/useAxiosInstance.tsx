b933de935c01f5fce22aca3d87831e1ad64bc573
koo-rogie <rn5184@naver.com>
2025-06-25 10:17:56 +0900

    🎨Feat : 인터셉터 추가

--- workspace/ch09-ajax/02-board-axios/src/hooks/useAxiosInstance.tsx
+++ workspace/ch09-ajax/02-board-axios/src/hooks/useAxiosInstance.tsx
@@ -1,19 +1,61 @@
 import axios from "axios";
 
-const API_SERVER = 'https://fesp-api.koyeb.app/market';
+const API_SERVER = "https://fesp-api.koyeb.app/market";
 
 function useAxiosInstance() {
   const instance = axios.create({
     baseURL: API_SERVER, // 기본 URL
-    timeout: 1000*5,
+    timeout: 1000 * 5,
     headers: {
-      'Content-Type': 'application/json', // 요청 바디의 데이터 타입
+      "Content-Type": "application/json", // 요청 바디의 데이터 타입
       // 설정하지 않으면 크롬일 경우 "application/json, text/plain, */*"
-      Accept: 'application/json', // 응답 바디의 데이터 타입이 json이면 좋겠음
-      'Client-Id': 'openmarket',
-    }
+      Accept: "application/json", // 응답 바디의 데이터 타입이 json이면 좋겠음
+      "Client-Id": "openmarket",
+    },
   });
 
+  // 요청 인터셉터 추가하기
+  instance.interceptors.request.use(
+    (config) => {
+      console.log("요청 인터셉터 호출", config);
+      // 요청이 전달되기 전에 필요한 공통 작업 수행
+      config.params = {
+        delay: 2000,
+        // 딜레이를 따로 주지 않아도 여기서 설정함,
+        // 다만 코드상에 딜레이를 줄 경우 코드에서 지정한 딜레이가 적용됨
+        ...config.params, // 기존 쿼리 스트링 복사
+      };
+      return config;
+    },
+    (error) => {
+      // 공통 에러 처리
+
+      return Promise.reject(error);
+    }
+  );
+
+  // 응답 인터셉터 추가하기
+  instance.interceptors.response.use(
+    (response) => {
+      console.log("정상응답 인터셉터 호출", response);
+      // 2xx 범위에 있는 상태 코드는 이 함수가 호출됨
+      // 응답 데이터를 이용해서 필요한 공통 작업 수행
+      if (response.data.ok !== undefined) {
+        response.data.ok = !!response.data.ok;
+      }
+
+      return response;
+    },
+    (error) => {
+      console.error("응답 실패", error);
+
+      // 2xx 외의 범위에 있는 상태 코드는 이 함수가 호출됨
+      // 공통 에러 처리
+
+      return Promise.reject(new Error("잠시 후 다시 이용해주시길 바랍니다.")); 
+      // 사용자에게는 에러코드를 적나하게 드러낼 이유가 없기때문에 이 작엄을 해줌
+    }
+  );
   return instance;
 }
 
