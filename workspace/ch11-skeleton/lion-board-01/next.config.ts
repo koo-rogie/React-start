7ece966ca217970b1800d3a94fdd0e4f6461c17c
koo-rogie <rn5184@naver.com>
2025-06-30 17:54:07 +0900

    🎨Feat : 외부 파일을 불러오기 위한 파일 수정

--- workspace/ch11-skeleton/lion-board-01/next.config.ts
+++ workspace/ch11-skeleton/lion-board-01/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fesp-api.koyeb.app",
+        port: "",
+        pathname: "/market/files/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
