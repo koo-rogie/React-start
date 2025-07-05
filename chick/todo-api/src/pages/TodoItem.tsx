626afb8e86c682f4aa6427489b197760f601a08a
koo-rogie <rn5184@naver.com>
2025-07-06 03:27:46 +0900

    🚧 work : 병아리반 임시 커밋

--- chick/todo-api/src/pages/TodoItem.tsx
+++ chick/todo-api/src/pages/TodoItem.tsx
@@ -1,3 +1,5 @@
+import useAxiosInstance from "../hooks/useAxiosInstance";
+
 export interface TodoItem {
   _id: number;
   title: string;
@@ -6,22 +8,30 @@ export interface TodoItem {
 
 interface TodoItemProps {
   item: TodoItem;
-  deleteItem: (_id: number) => void;
-  toggleDone: (_id: number) => void;
+  fetchList: () => void;
 }
 
-function TodoItem({ item, deleteItem, toggleDone }: TodoItemProps) {
+function TodoItem({ item, fetchList }: TodoItemProps) {
+  const axiosInstance = useAxiosInstance();
 
-  const handleDelete = (_id: number) => {
-    console.log(_id, '삭제 요청.');
-    deleteItem(_id);
+  const handleDelete = async (_id: number) => {
+    console.log(_id, "삭제 요청.");
+
+    try {
+      await axiosInstance.delete(`/todolist/${item._id}`);
+      fetchList(); // 여기 지워야함...ㅋㅋㅋ
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
     <li>
-      <span>{ item._id }</span>
-      <span onClick={ () => toggleDone(item._id) }>{ item.done ? <s>{ item.title }</s> : item.title }</span>
-      <button type="button" onClick={ () => handleDelete(item._id) }>삭제</button>
+      <span>{item._id}</span>
+      <span onClick={() => fetchList()}>{item.done ? <s>{item.title}</s> : item.title}</span>
+      <button type="button" onClick={() => handleDelete(item._id)}>
+        삭제
+      </button>
     </li>
   );
 }
--- chick/todo-api/src/pages/TodoList.tsx
+++ chick/todo-api/src/pages/TodoList.tsx
@@ -3,28 +3,14 @@ import useAxiosInstance from "../hooks/useAxiosInstance";
 import TodoItem, { type TodoItem as TodoItemType } from "./TodoItem";
 
 interface TodoListPropType {
-  itemList: TodoItemType[];
   deleteItem: (_id: number) => void;
   toggleDone: (_id: number) => void;
 }
 
-function TodoList({ itemList, deleteItem, toggleDone }: TodoListPropType) {
+function TodoList({ deleteItem, toggleDone }: TodoListPropType) {
   const axiosInstance = useAxiosInstance();
-  // const [itemList, setItemList] = useState();
 
-  // 서버로부터 목록 수신
-  // const fetchList = () => {
-  //   const res = axiosInstance
-  //     .get('/todolist')
-  //     .then(() => {}) //fulfilled;
-  //     .catch(() => {}); //rejected
-  //   console.log(`서버의 응답`, res);
-
-  //   // res는 Promise 객체로 반환
-  //   // 1. pending 작업 진행 중 → 로딩 스피너나 스켈레톤 UI 표시
-  //   // 2. fulfilled 작업성공
-  //   // 3. rejected 작업실패
-  // };
+  const [itemList, setItemList] = useState<TodoItemType[]>([]);
 
   const fetchList = async () => {
     /** Promise 상태
@@ -35,25 +21,32 @@ function TodoList({ itemList, deleteItem, toggleDone }: TodoListPropType) {
 
     try {
       // 성공했을때 (fulfilled 상태)
-      const res = await axiosInstance.get("/todolist");
+      const res = await axiosInstance.get("/todolist"); // fulfilled 상태
       console.log("서버의 전체 응답", res.data);
       console.log("서버의 data 속성만 응답", res.data);
       console.log("서버의 data.items 속성만 응답", res.data.items);
+
+      setItemList(res.data.items);
     } catch (err) {
       // 응답에 실패했을때 (rejected
       console.error(err);
     }
   };
 
+  // 최초에만 서버에서만 받아올 수 있도록!
   useEffect(() => {
     fetchList();
   }, []);
 
   const liList = itemList.map((item) => {
-    return <TodoItem key={item._id} item={item} deleteItem={deleteItem} toggleDone={toggleDone} />;
+    return <TodoItem key={item._id} item={item} fetchList={fetchList} />;
   });
 
-  return <ul className="todolist">{liList}</ul>;
+  return (
+    <ul className="todolist">
+      <>{liList}</>
+    </ul>
+  );
 }
 
 export default TodoList;
