eef25cd19745c269938536169c9f4554ff6ee044
koo-rogie <rn5184@naver.com>
2025-07-06 03:21:37 +0900

    🎨Feat : 서버 키를 상수로 분리

--- chick/todo-api/src/hooks/useAxiosInstance.ts
+++ chick/todo-api/src/hooks/useAxiosInstance.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const API_SERVER = "https://fesp-api.koyeb.app/todo";
+
 export default function useAxiosInstance() {
-  // 초기값 세팅
   const instance = axios.create({
-    baseURL: "https://fesp-api.koyeb.app/todo/",
-    timeout: 1000 * 5, // 응답이 `timeout(밀리초)`보다 오래 걸리면 요청이 중단되고 timeout 에러 발생
+    // create에는 객체형태로 전달됨
+    baseURL: API_SERVER,
+    timeout: 1000 * 5,
     headers: {
       "Content-Type": "application/json", // 요청 바디의 데이터 타입
-      Accept: "application/json", // 기대하는 응답 데이터 타입
+      Accept: "application/json", // 서버로부터 기대하는 응답 데이터 타입
     },
   });
 
